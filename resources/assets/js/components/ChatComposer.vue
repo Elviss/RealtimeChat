4f86224eee45b9776121489579616667caade149
Elvis <slv_evs@yahoo.com.br>
2017-12-31 21:02:02 -0200

    criacao do componente chat-composer

---
+++ resources/assets/js/components/ChatComposer.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="chat-composer">
+            
+                <input type="text" placeholder="Start typing your message...">
+                    
+                <button class="btn btn-primary">Send</button>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            
+        }
+    }
+</script>
+
+<style type="text/css">
+    .chat-composer {
+        display: flex;
+    }
+    .chat-composer input {
+        flex: 1 auto;
+    }
+    
+    .chat-composer button {
+        border-radius: 0;
+    }
+</style>
