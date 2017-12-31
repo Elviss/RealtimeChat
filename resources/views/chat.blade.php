f49c29f2db5497995742e5c64c8b594cd6bda3b6
Elvis <slv_evs@yahoo.com.br>
2017-12-31 19:56:26 -0200

    iniciando a pagina do chat e o componente vue

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>Chatroom</title>
+</head>
+
+<link rel="stylesheet" href="css/app.css">
+
+<body>
+
+<div id="app">
+    <h1>Chatroom</h1>
+
+    <chat-message></chat-message>
+    
+    <chat-log></chat-log>
+    
+    <chat-composer></chat-composer>
+</div>
+
+<script src="js/app.js" charset="utf-8"></script>
+</body>
+</html>
