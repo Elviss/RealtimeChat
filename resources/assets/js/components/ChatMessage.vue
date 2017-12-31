f7ac02bd29c8cb61475dcddf0ba7eb7bfa4e4689
Elvis <slv_evs@yahoo.com.br>
2017-12-31 20:35:08 -0200

    criacao do componente chat-message

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <p>{{ message }}</p>
+
+                <small>{{ user }}</small>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "This is some message text",
+                user: "Elvis"
+            }      
+        }
+    }
+</script>
